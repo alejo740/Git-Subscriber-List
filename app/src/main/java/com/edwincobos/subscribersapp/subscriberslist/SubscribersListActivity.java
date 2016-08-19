090c9c2363305f28d6a368ddab51ad783357972d
Edwin Alejandro Cobos Fonseca <alejo740@gmail.com>
2016-08-19 13:55:54 -0500

    SPLASH SCREEN
    Implemented splash screen with logo animation and basic transition to
    the empty Subscriptors List Activity

---
+++ app/src/main/java/com/edwincobos/subscribersapp/subscriberslist/SubscribersListActivity.java
@@ -0,0 +1,15 @@
+package com.edwincobos.subscribersapp.subscriberslist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.edwincobos.subscribersapp.R;
+
+public class SubscribersListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscribers_list);
+    }
+}
