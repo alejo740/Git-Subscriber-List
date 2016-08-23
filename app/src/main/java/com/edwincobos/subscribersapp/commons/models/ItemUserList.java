f1245ef49bfd2ae37e6886c14634763616545ae0
Edwin Alejandro Cobos Fonseca <alejo740@gmail.com>
2016-08-23 11:13:09 -0500

    Fixed ItemList
    Fixed model of itemList with each attribute from Json to parser the
    object.

--- app/src/main/java/com/edwincobos/subscribersapp/commons/models/ItemUserList.java
+++ app/src/main/java/com/edwincobos/subscribersapp/commons/models/ItemUserList.java
@@ -7,101 +7,394 @@ import java.util.ArrayList;
  */
 public class ItemUserList {
 
-    private int id;
-
-    //@SerializedName("login")
-    private String userName;
-
-    private String name;
-    private String company;
-    private String location;
-    private String avatarUrl;
-    private int followersCounter;
-    private int followingCounter;
-    private int reposCounter;
     private ArrayList<String> repoNames;
+    private String login;
+    private int id;
+    private String avatarUrl;
+    private String gravatarId;
+    private String url;
+    private String htmlUrl;
+    private String followersUrl;
+    private String followingUrl;
+    private String gistsUrl;
+    private String starredUrl;
+    private String subscriptionsUrl;
+    private String organizationsUrl;
+    private String reposUrl;
+    private String eventsUrl;
+    private String receivedEventsUrl;
+    private String type;
+    private boolean siteAdmin;
 
-    public ItemUserList(int id, String userName){
-        this.id = id;
-        this.userName = userName;
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public ItemUserList() {
     }
 
+    /**
+     *
+     * @param repoNames
+     * @param login
+     * @param id
+     * @param avatarUrl
+     * @param gravatarId
+     * @param url
+     * @param htmlUrl
+     * @param followersUrl
+     * @param followingUrl
+     * @param gistsUrl
+     * @param starredUrl
+     * @param subscriptionsUrl
+     * @param organizationsUrl
+     * @param reposUrl
+     * @param eventsUrl
+     * @param receivedEventsUrl
+     * @param type
+     * @param siteAdmin
+     */
+    public ItemUserList(ArrayList<String> repoNames, String login, int id, String avatarUrl, String gravatarId, String url, String htmlUrl, String followersUrl, String followingUrl, String gistsUrl, String starredUrl, String subscriptionsUrl, String organizationsUrl, String reposUrl, String eventsUrl, String receivedEventsUrl, String type, boolean siteAdmin) {
+        this.repoNames = repoNames;
+        this.login = login;
+        this.id = id;
+        this.avatarUrl = avatarUrl;
+        this.gravatarId = gravatarId;
+        this.url = url;
+        this.htmlUrl = htmlUrl;
+        this.followersUrl = followersUrl;
+        this.followingUrl = followingUrl;
+        this.gistsUrl = gistsUrl;
+        this.starredUrl = starredUrl;
+        this.subscriptionsUrl = subscriptionsUrl;
+        this.organizationsUrl = organizationsUrl;
+        this.reposUrl = reposUrl;
+        this.eventsUrl = eventsUrl;
+        this.receivedEventsUrl = receivedEventsUrl;
+        this.type = type;
+        this.siteAdmin = siteAdmin;
+    }
+
+    /**
+     *
+     * @return
+     * The login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     *
+     * @param login
+     * The login
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     *
+     * @return
+     * The id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     *
+     * @param id
+     * The id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     *
+     * @return
+     * The avatarUrl
+     */
     public String getAvatarUrl() {
         return avatarUrl;
     }
 
+    /**
+     *
+     * @param avatarUrl
+     * The avatar_url
+     */
     public void setAvatarUrl(String avatarUrl) {
         this.avatarUrl = avatarUrl;
     }
 
-    public String getName() {
-        return name;
+    /**
+     *
+     * @return
+     * The gravatarId
+     */
+    public String getGravatarId() {
+        return gravatarId;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    /**
+     *
+     * @param gravatarId
+     * The gravatar_id
+     */
+    public void setGravatarId(String gravatarId) {
+        this.gravatarId = gravatarId;
     }
 
-    public String getUserName() {
-        return userName;
+    /**
+     *
+     * @return
+     * The url
+     */
+    public String getUrl() {
+        return url;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    /**
+     *
+     * @param url
+     * The url
+     */
+    public void setUrl(String url) {
+        this.url = url;
     }
 
-    public String getCompany() {
-        return company;
+    /**
+     *
+     * @return
+     * The htmlUrl
+     */
+    public String getHtmlUrl() {
+        return htmlUrl;
     }
 
-    public void setCompany(String company) {
-        this.company = company;
+    /**
+     *
+     * @param htmlUrl
+     * The html_url
+     */
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
     }
 
-    public String getLocation() {
-        return location;
+    /**
+     *
+     * @return
+     * The followersUrl
+     */
+    public String getFollowersUrl() {
+        return followersUrl;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+    /**
+     *
+     * @param followersUrl
+     * The followers_url
+     */
+    public void setFollowersUrl(String followersUrl) {
+        this.followersUrl = followersUrl;
     }
 
-    public int getFollowersCounter() {
-        return followersCounter;
+    /**
+     *
+     * @return
+     * The followingUrl
+     */
+    public String getFollowingUrl() {
+        return followingUrl;
     }
 
-    public void setFollowersCounter(int followersCounter) {
-        this.followersCounter = followersCounter;
+    /**
+     *
+     * @param followingUrl
+     * The following_url
+     */
+    public void setFollowingUrl(String followingUrl) {
+        this.followingUrl = followingUrl;
     }
 
-    public int getFollowingCounter() {
-        return followingCounter;
+    /**
+     *
+     * @return
+     * The gistsUrl
+     */
+    public String getGistsUrl() {
+        return gistsUrl;
     }
 
-    public void setFollowingCounter(int followingCounter) {
-        this.followingCounter = followingCounter;
+    /**
+     *
+     * @param gistsUrl
+     * The gists_url
+     */
+    public void setGistsUrl(String gistsUrl) {
+        this.gistsUrl = gistsUrl;
     }
 
-    public int getReposCounter() {
-        return reposCounter;
+    /**
+     *
+     * @return
+     * The starredUrl
+     */
+    public String getStarredUrl() {
+        return starredUrl;
     }
 
-    public void setReposCounter(int reposCounter) {
-        this.reposCounter = reposCounter;
+    /**
+     *
+     * @param starredUrl
+     * The starred_url
+     */
+    public void setStarredUrl(String starredUrl) {
+        this.starredUrl = starredUrl;
     }
 
+    /**
+     *
+     * @return
+     * The subscriptionsUrl
+     */
+    public String getSubscriptionsUrl() {
+        return subscriptionsUrl;
+    }
+
+    /**
+     *
+     * @param subscriptionsUrl
+     * The subscriptions_url
+     */
+    public void setSubscriptionsUrl(String subscriptionsUrl) {
+        this.subscriptionsUrl = subscriptionsUrl;
+    }
+
+    /**
+     *
+     * @return
+     * The organizationsUrl
+     */
+    public String getOrganizationsUrl() {
+        return organizationsUrl;
+    }
+
+    /**
+     *
+     * @param organizationsUrl
+     * The organizations_url
+     */
+    public void setOrganizationsUrl(String organizationsUrl) {
+        this.organizationsUrl = organizationsUrl;
+    }
+
+    /**
+     *
+     * @return
+     * The reposUrl
+     */
+    public String getReposUrl() {
+        return reposUrl;
+    }
+
+    /**
+     *
+     * @param reposUrl
+     * The repos_url
+     */
+    public void setReposUrl(String reposUrl) {
+        this.reposUrl = reposUrl;
+    }
+
+    /**
+     *
+     * @return
+     * The eventsUrl
+     */
+    public String getEventsUrl() {
+        return eventsUrl;
+    }
+
+    /**
+     *
+     * @param eventsUrl
+     * The events_url
+     */
+    public void setEventsUrl(String eventsUrl) {
+        this.eventsUrl = eventsUrl;
+    }
+
+    /**
+     *
+     * @return
+     * The receivedEventsUrl
+     */
+    public String getReceivedEventsUrl() {
+        return receivedEventsUrl;
+    }
+
+    /**
+     *
+     * @param receivedEventsUrl
+     * The received_events_url
+     */
+    public void setReceivedEventsUrl(String receivedEventsUrl) {
+        this.receivedEventsUrl = receivedEventsUrl;
+    }
+
+    /**
+     *
+     * @return
+     * The type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     *
+     * @param type
+     * The type
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     *
+     * @return
+     * The siteAdmin
+     */
+    public boolean isSiteAdmin() {
+        return siteAdmin;
+    }
+
+    /**
+     *
+     * @param siteAdmin
+     * The site_admin
+     */
+    public void setSiteAdmin(boolean siteAdmin) {
+        this.siteAdmin = siteAdmin;
+    }
+
+    /**
+     *
+     * @return
+     * The repoNames
+     */
     public ArrayList<String> getRepoNames() {
         return repoNames;
     }
 
+    /**
+     *
+     * @param repoNames
+     * The repo_names
+     */
     public void setRepoNames(ArrayList<String> repoNames) {
         this.repoNames = repoNames;
     }
